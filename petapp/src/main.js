5529bd43bedb5a7109576959cb15ce33c5a92709
music <m0911k163.com>
2019-10-14 02:12:18 +0800

    配置mint-ui和vant

--- petapp/src/main.js
+++ petapp/src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入mint-ui组件库中的所有组件
+import MintUI from 'mint-ui'
+// 引入mint-ui库中样式文件
+import 'mint-ui/lib/style.css'
+// 注册mint-ui
+Vue.use(MintUI)
+// 引入vant组件库
+import Vant from 'vant'
+// 引入vant库中的样式文件
+import 'vant/lib/index.css'
+// 注册vant
+Vue.use(Vant);
 // 引入axios模块
 import axios from "axios"
 
