6034a8a987f0ad39357a7f3ed68642a9d24f0fdd
zy <zy@163.com>
2019-10-14 13:28:23 +0800

    zy01

--- petapp/src/router.js
+++ petapp/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+import Index from './components/Index.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -18,6 +20,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {path:'/Index',name:'Index',component:Index}
   ]
 })
