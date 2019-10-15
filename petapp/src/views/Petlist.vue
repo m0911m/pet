13fc5b18aa421f8a505221cc7a01c1628e7a1259
chm <chm@163.com>
2019-10-15 09:54:29 +0800

    agfajh

---
+++ petapp/src/views/Petlist.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <van-icon name="arrow-left" />
+      </div>
+      <div>
+        宠物列表
+      </div>
+      <div>
+        <van-icon name="plus" />
+      </div>
+    </div>
+    <hr>
+  </div>
+</template>
