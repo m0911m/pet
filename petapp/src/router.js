a56ceb7e0e408fa1b3a1aee158bb528d95c29060
zy <zy@163.com>
2019-10-17 09:55:23 +0800

    detailstory

--- petapp/src/router.js
+++ petapp/src/router.js
@@ -17,7 +17,7 @@ import Register from './views/Register.vue'
 //引入宠物品种组件
 import Kind from './views/Kind.vue'
 // 引入动态详情组件
-import Detailstory from './views/Detailstory.vue'
+// import Detailstory from './views/Detailstory.vue'
 
 
 
@@ -28,7 +28,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {path:'/Kind',component:Kind},
-    {path:'/Detailstory',component:Detailstory},
+    // {path:'/Detailstory',component:Detailstory},
     {path:'/Register',component:Register},
     {path:'/Newstory',component:Newstory},
     {path:'/Swipe',component:Swipe},
