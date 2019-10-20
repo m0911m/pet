fd46537e9ba90070d8f5327d6a1f3cf9fd1b01f4
m0911m <m0911k@163.com>
2019-10-20 16:00:24 +0800

    1231

--- petapp/src/views/Login.vue
+++ petapp/src/views/Login.vue
@@ -79,16 +79,12 @@ export default {
   display:flex;
   position:relative;
   width:100%;
-  /* position:relative; */
   text-align: center;
   box-sizing: border-box;
-  /* padding-top:4.875rem;
-  padding-bottom:8rem; */
-  /* background: url('../../public/imgs/login_bg.jpg') no-repeat; */
-  /* background-size:cover; */
 }
 /* 背景 */
 .imgstyle{
+  position:relative;
   width:100%;
   height:100%;
 }
