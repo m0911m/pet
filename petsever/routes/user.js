f33a986ef0afabd18116df426b3daae674936e0c
yx <yx@163.com>
2019-10-21 20:55:31 +0800

    20:55

--- petsever/routes/user.js
+++ petsever/routes/user.js
@@ -4,7 +4,7 @@ const express=require('express');
 const pool=require('../pool.js');
 //创建路由器对象
 var router=express.Router();
-// 一、用户登录模块
+// 一、用户登录模块  
 router.post("/login",(req,res)=>{
 	var uname=req.body.uname;
 	var upwd=req.body.upwd;
@@ -23,7 +23,7 @@ router.post("/login",(req,res)=>{
 	})
 })
 
-//二、用户注册模块
+//二、用户注册模块   验证过
 //2.1用户注册验证用户名手机号是否可用
 router.get("/isreg",(req,res)=>{
 	var uname=req.query.uname;
@@ -51,7 +51,7 @@ router.post("/reg",(req,res)=>{
 	})
 })
 
-//三、动态发布模块
+//三、动态发布模块   验证过
 router.post("/updatamessagelist",(req,res)=>{
 	//获取发布动态用户的uid
 	var uid=req.session.uid;
@@ -74,7 +74,7 @@ pool.query(sql,(err,result)=>{
 }
 })
 });
-//四、动态页浏览模块
+//四、动态页浏览模块     验证过
 router.get("/messagelist",(req,res)=>{
 var sql="SELECT tid,ttitle,tsmtitle,ttxt,t_img,uid FROM cw_text";
 pool.query(sql,(err,result)=>{
@@ -86,14 +86,14 @@ pool.query(sql,(err,result)=>{
 		}
 })
 });
-//五、宠物信息添加
+//五、宠物信息添加   验证过
 router.post("/updatapetmessage",(req,res)=>{
 	//获取发布动态用户的uid
-	var uid=req.session.uid;
- if(!uid){
- res.send({code:-2,msg:"请登录"});
- 	return;
- }
+ 	var uid=req.session.uid;
+  if(!uid){
+  res.send({code:-2,msg:"请登录"});
+  	return;
+  }
  //获取宠物物信息
 	var animal_type=req.body.animal_type;
 	var animal_age=req.body.animal_age;
@@ -110,11 +110,33 @@ router.post("/updatapetmessage",(req,res)=>{
 		}
 	})
 })
-//六、修改宠物信息
+//六、查询宠物信息  验证过
+router.get("/selectpetmessage",(req,res)=>{
+	//获取发布动态用户的uid
+ 	var uid=req.session.uid;
+  if(!uid){
+  res.send({code:-2,msg:"请登录"});
+  	return;
+  }
+ //获取宠物物信息
+	// var animal_type=req.query.animal_type;
+	// var animal_age=req.query.animal_age;
+	// var animal_sex=req.query.animal_sex;
+	// var animal_weight=req.query.animal_weight;
+	// var animal_img=req.query.animal_img;
+	var sql="SELECT  aid,animal_type,animal_age,animal_sex,animal_weight,animal_img FROM cw_animal WHERE uid=?";
+	pool.query(sql,[uid],(err,result)=>{
+		if(err)throw err;
+		if(result.length>0){
+			res.send({code:1,msg:"宠物信息查询成功",data:result})
+		}else{
+			res.send({code:-1,msg:"宠物信息查询失败"})
+		}
+	})
+})
 
 
-
-//七、首页故事模块
+//七、首页故事模块  验证过
 router.get("/indexstory",(req,res)=>{
 	var sql="SELECT iid,uid,ititle,iuname,ismtitle,itxt,itime,i_img,i_icon FROM cw_sindex";
 	pool.query(sql,(err,result)=>{
