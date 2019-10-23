992c66415478b918e14bb2bb6eb39fb5d70adb92
m0911m <m0911k@163.com>
2019-10-23 20:51:39 +0800

    1233

--- petapp/src/components/Navbar.vue
+++ petapp/src/components/Navbar.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    
+    <mt-header fixed>
+      <router-link :to="{path:'/Nav'}" slot="left">
+        <mt-button icon="back"></mt-button>
+      </router-link>
+        <mt-button slot="left">西安</mt-button>
+        <input slot="left" type="text" placeholder="选择寄养时间">
+        <mt-button slot="right">
+          <van-icon name="location-o"></van-icon>
+        </mt-button>
+    </mt-header>
   </div>
 </template>
 <script>
@@ -9,6 +18,11 @@ export default {
 }
 </script>
 <style scoped>
-
+.mint-header.is-fixed {
+  width:100%;
+  height:3rem;
+  padding:0;
+  background-color:#f2f2f2;
+}
 
 </style>
