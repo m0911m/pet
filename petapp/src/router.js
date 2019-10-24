2b60c2ddcf505bf25b09cfe54e577e7ab3f933c5
Mia-zy150614 <zhangyue3699@163.com>
2019-10-25 01:25:07 +0800

    客服消息

--- petapp/src/router.js
+++ petapp/src/router.js
@@ -42,13 +42,18 @@ import Navbar from './components/Navbar.vue'
 import Details from './views/Details.vue'
 // 引入消息组件
 import Message from './views/Message.vue'
-
+// 公告详情页
+import Announcement from './views/Announcement.vue'
+// 客服消息页
+import Service from './views/Service.vue'
 
 Vue.use(Router)
 
 
 export default new Router({
   routes: [
+    {path:"/Service",component:Service},
+    {path:"/Announcement",component:Announcement},
     {path:"/Message",component:Message},
     {path:"/Details",component:Details},
     {path:"/Navbar",component:Navbar},
---
+++ petapp/src/views/Service.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <!-- 顶部导航栏 -->
+    <van-nav-bar title="客服消息" left-arrow @click-right="onClickRight">
+      <van-icon name="phone-circle-o" slot="right" size="25px" />
+    </van-nav-bar>
+    <!-- 中间的聊天区域 -->
+    <div class="chat">
+      1111111111111111
+    </div>
+    <!-- 底部输入框 -->
+    <div class="downfixed">
+      <!-- 输入框 -->
+      <div class="down">
+        <input type="text" placeholder="输入新消息..." class="inputmsg">
+        <van-icon name="smile-o" size="25px"/>
+        <van-icon name="add-o" size="25px"/>
+      </div>
+      <!-- 选择弹出框 -->
+      <div class="grid">
+        <van-grid>
+          <van-grid-item icon="photo-o" text="图片" />
+        </van-grid>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    onClickRight(){
+      this.$dialog.confirm({
+        title: '温馨提示',
+        message: '拨打客服热线：000-100-000'
+      }).then(() => {
+        // on confirm
+      }).catch(() => {
+        // on cancel
+      });
+    }
+  }
+}
+</script>
+<style scoped>
+.container{
+  position: relative;
+}
+.downfixed{
+  display: absolute;
+  bottom:0;
+}
+.chat{
+  width:100%;
+  height:672px;
+}
+.down{
+  height:30px;
+  width:100%;
+  padding:10px;
+  border-top:1px solid #ccc;
+  
+  display:flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.inputmsg{
+  border-radius: 5px;
+  padding:3px;
+  outline: 0;
+  border:1px solid #ccc;
+  width:70%;
+}
+.grid{
+  width:100%;
+  display:none;
+}
+</style>
