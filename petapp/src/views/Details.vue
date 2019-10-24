0abc7b02f60548b9c5e1b45435705bfa56e003d3
m0911m <m0911k@163.com>
2019-10-24 17:16:35 +0800

    123123

--- petapp/src/views/Details.vue
+++ petapp/src/views/Details.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
     <heads></heads>
-    <foster></foster>
+    <foster :homelist="homelist"></foster>
   </div>
 </template>
 <script>
 // 引入head导航组件
 import Head from "../components/Head.vue"
 // 引入用户家庭组件
-import Foster from "../components/Foster"
+import Foster from "../components/Foster.vue"
 export default {
   data(){
     return{
-
+      homelist:[
+        {
+          h_img:"family0.jpg",
+          u_img:"family_user0.jpg",
+          num:35,
+          u:"小福的家",
+          score:"5.0",
+          sit:"新科大厦",
+          gaps:"0.3km"
+        }
+      ]
     }
   },
   methods:{
