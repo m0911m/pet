26720f01a52397d4e24f489007525a7df4f1abb7
music <m0911k163.com>
2019-10-24 08:07:11 +0800

    1111.

--- petapp/src/components/Navbar.vue
+++ petapp/src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <mt-header fixed>
+    <mt-header class="header" fixed>
       <router-link :to="{path:'/Nav'}" slot="left">
         <mt-button icon="back"></mt-button>
       </router-link>
         <mt-button slot="left">西安</mt-button>
-        <input slot="left" type="text" placeholder="选择寄养时间">
+        <mt-button slot="right">选择寄养时间</mt-button>
         <mt-button slot="right">
           <van-icon name="location-o"></van-icon>
         </mt-button>
@@ -17,12 +17,24 @@ export default {
   
 }
 </script>
-<style scoped>
+<style>
+/* 最外层 */
 .mint-header.is-fixed {
   width:100%;
-  height:3rem;
+  height:4rem;
   padding:0;
   background-color:#f2f2f2;
+  color:#040404;
 }
-
+/* 左侧按钮 */
+.mintui-back:before{
+ padding-left:1rem;
+ font-size:1.875rem;
+} 
+/* 市区 */
+label{
+  padding-left:1.5rem;
+  font-size:1rem;
+  widows: 100px;
+}
 </style>
