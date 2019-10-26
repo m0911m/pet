493984afa85afb23029e8193250d503972cd734e
yx <yx@163.com>
2019-10-26 11:23:06 +0800

    12416

--- petsever/routes/foster.js
+++ petsever/routes/foster.js
@@ -17,7 +17,32 @@ router.get("/fosterlist",(req,res)=>{
 
 	})
 })
-//二、申请成为寄养家庭
-router.post()
+// //二、申请成为寄养家庭
+// router.post("/newfosterhome",(req,res)=>{
+
+
+// 	var uid=req.session.uid;
+//   if(!uid){
+//   res.send({code:402,msg:"请登录"});
+//   	return;
+//   }
+//  //获取寄养家庭信息
+// 	var ftitle=req.body.ftitle;
+// 	var fprice=req.body.fprice;
+// 	var fscore=req.body.fscore;
+// 	var fisonbuy=req.body.fisonbuy;
+// 	var faddress=req.body.faddress;
+// 	var f_img=req.body.f_img;
+// 	// var sql=`INSERT INTO cw_animal VALUES(NULL,'${uid}','${animal_type}','${animal_age}',${animal_sex},'${animal_weight}','${animal_img}')`;
+// 	// pool.query(sql,(err,result)=>{
+// 	// 	if(err)throw err;
+// 	// 	if(result.affectedRows>0){
+// 	// 		res.send({code:200,msg:"宠物信息添加成功"})
+// 	// 	}else{
+// 	// 		res.send({code:401,msg:"宠物信息添加查询失败"})
+// 		}
+// 	})
+
+// })
 
 module.exports=router;
