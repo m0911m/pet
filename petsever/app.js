bb7b4be56d8b061cb53cae699389f71d2886bd23
yx <yx@163.com>
2019-10-28 18:50:27 +0800

    222222

--- petsever/app.js
+++ petsever/app.js
@@ -16,45 +16,46 @@ const cors=require("cors");
 
 //使用express模块
 var app=express();
-//监听端口
-app.listen(5050);
+
+
+app.use(cookieParser());
+//配置session 
+app.use(session({ 
+// secret:"128位字符", 
+secret: 'sessiontest', 
+cookie:{maxAge:10000},
+resave:true, 
+saveUninitialized:false
+})); 
 
 app.use(cors({ 
-	origin:["http://127.0.0.1:5050","http://localhost:8080"], 
+	origin:["http://127.0.0.1:8080","http://localhost:8080"], 
 	credentials:true, 
 	methods:['GET','POST'], 
 	alloweHeaders:['Conten-Type', 'Authorization'] 
-	})); 
+})); 
 //  app.use(cookieParser('sessiontest'));
-	 //配置session 
-	app.use(session({ 
-	// secret:"128位字符", 
-	secret: 'sessiontest', 
-	cokkie:{maxAge:10000},
-	resave:true, 
-	saveUninitialized:false
-	})); 
- //使用body-parser中间件 
- app.use(bodyParser.urlencoded({extended:false})); 
+
 
 // //配置跨域
 // app.use(cors({
-// 	origin:["http://127.0.0.1:5050","http://localhost:8080"],
-// 	credentials:true
-// }));
-//设置跨域访问  
-// app.all('/', function(req, res, next) {  
-// 	next();  
-// 	res.header("Access-Control-Allow-Origin", "true");  
-// 	res.header("Access-Control-Allow-Headers", "X-Requested-With");  
-// 	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
-// 	res.header("X-Powered-By",' 3.2.1')  
-// 	res.header("Content-Type", "application/json;charset=utf-8");  
-// });  
-
-
+	// 	origin:["http://127.0.0.1:5050","http://localhost:8080"],
+	// 	credentials:true
+	// }));
+	//设置跨域访问  
+	// app.all('/', function(req, res, next) {  
+		// 	next();  
+		// 	res.header("Access-Control-Allow-Origin", "true");  
+		// 	res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+		// 	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+		// 	res.header("X-Powered-By",' 3.2.1')  
+		// 	res.header("Content-Type", "application/json;charset=utf-8");  
+		// });  
+		
+		
 //使用body-parser中间件
 app.use(bodyParser.urlencoded({extended:false}));
+
 //托管静态资源到public目录下
 app.use(express.static("public"));
 
@@ -71,3 +72,6 @@ app.use(express.static("public"));
 app.use( '/user',userRouter );
 app.use( '/foster',fosterRouter );
 app.use( '/news',newsRouter );
+
+//监听端口
+app.listen(5050);
